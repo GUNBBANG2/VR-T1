78fdb6f3b547b4a03f921a34ced56496bdce071d
GUNBBANG2 <rjsdnrsla123@gmail.com>
2024-10-16 11:19:33 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,196 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-3",
+      "name": "파노라마3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.6464982925680882,
+          "pitch": -0.29855507981788065,
+          "title": "상가 전면",
+          "text": "XXX 상가의 정면 디자인<div>-심플함, 노멀함 을 강조한 파사드</div><div>주간 및 야간 어디서나 접근이 용이함</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-5",
+      "name": "파노라마5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1386926362091998,
+          "pitch": -0.02215625407679056,
+          "rotation": 0,
+          "target": "2-6"
+        },
+        {
+          "yaw": -1.4478761613473452,
+          "pitch": -0.030247588865099928,
+          "rotation": 0,
+          "target": "0-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4437603881465684,
+          "pitch": -0.15929762323370333,
+          "title": "최초 페이지로 가시지용",
+          "text": "최초 뷰로 이동하시려면 누르세염화나트륨"
+        }
+      ]
+    },
+    {
+      "id": "2-6",
+      "name": "파노라마6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 1.8976862164175659,
+        "pitch": 0.2468388226660032,
+        "fov": 1.4999314637932997
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.183226791097468,
+          "pitch": -0.07660433825247281,
+          "title": "주방 및 POS",
+          "text": "계산 및 울랄라 울랄라"
+        },
+        {
+          "yaw": 1.5978691852432725,
+          "pitch": -0.0041409860232271,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-7",
+      "name": "파노라마7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-8",
+      "name": "파노라마8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.5495443794866279,
+          "pitch": -0.12834389224518716,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-9",
+      "name": "파노라마9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
